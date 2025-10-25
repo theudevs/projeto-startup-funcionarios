f9a3673fb51ecfdee2412bd9324f72d22780ff28
Matheus Alves <matheusalvesseverino1411@gmail.com>
2025-10-24 21:55:12 -0300

    Exercício 4 – Relatório com Métodos de Array (map, filter, reduce)

--- script.js
+++ script.js
@@ -37,6 +37,13 @@ const inputSalario = document.getElementById('salario');
 const mensagem = document.getElementById('mensagem');
 const btnSalvar = document.getElementById('btn-salvar');
 
+const btnRelatorioSalario = document.getElementById('btn-relatorio-salario');
+const btnRelatorioMedia = document.getElementById('btn-relatorio-media-salarial');
+const btnRelatorioCargos = document.getElementById('btn-relatorio-cargos');
+const btnRelatorioNomes = document.getElementById('btn-relatorio-nomes-maiusculos');
+const areaRelatorio = document.getElementById('area-relatorio');
+const tipoRelatorio = document.getElementById('tipo-relatorio');
+
 form.addEventListener('submit', (event) =>{
     event.preventDefault();
 
@@ -127,3 +134,58 @@ function renderTabela(){
         tbody.appendChild(tr);
     }
 }
+
+btnRelatorioSalario.addEventListener('click', () => {
+    areaRelatorio.innerHTML = '';
+    tipoRelatorio.textContent = 'Funcionários com salário > R$ 5000,00';
+    if(!funcionarios.length){
+        areaRelatorio.textContent = 'Não há funcionários cadastrados';
+        return;
+    }
+    const salariosAltos = funcionarios.filter((func) => func.getSalario() > 5000);
+
+    if(salariosAltos.length === 0){
+        areaRelatorio.textContent = 'Nenhum funcionário com salário > R$ 5000,00';
+        return;
+    }
+
+    const linhas = salariosAltos.map((func) => func.toString()).join('\n');
+    areaRelatorio.textContent = linhas;
+});
+btnRelatorioMedia.addEventListener('click', () => {
+    areaRelatorio.innerHTML = '';
+    tipoRelatorio.textContent = 'Média salarial dos funcionários';
+    if(!funcionarios.length){
+        areaRelatorio.textContent = 'Não há funcionários cadastrados';
+        return;
+    }
+    const somaTotal = funcionarios.reduce((acc, func) => {
+        return acc + func.getSalario();
+    }, 0);
+
+    const media = somaTotal / funcionarios.length;
+    areaRelatorio.textContent = `R$ ${media.toFixed(2)}`;
+});
+btnRelatorioCargos.addEventListener('click', () => {
+    areaRelatorio.innerHTML = '';
+    tipoRelatorio.textContent = 'Cargos Únicos';
+    if(!funcionarios.length){
+        areaRelatorio.textContent = 'Não há funcionários cadastrados';
+        return;
+    }
+    const todosOsCargos = funcionarios.map((func) => func.getCargo());
+
+    const cargosUnidos = [...new Set(todosOsCargos)];
+
+    areaRelatorio.textContent = cargosUnidos.join('\n');
+});
+btnRelatorioNomes.addEventListener('click', () => {
+    areaRelatorio.innerHTML = '';
+    tipoRelatorio.textContent = 'Nomes em maiúsculo';
+    if(!funcionarios.length){
+        areaRelatorio.textContent = 'Não há funcionários cadastrados';
+        return;
+    }
+    const nomesMaiusculos = funcionarios.map((func) => func.getNome().toUpperCase());
+    areaRelatorio.textContent = nomesMaiusculos.join('\n');
+});
